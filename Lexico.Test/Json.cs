f57fe388a7b4d33403c805e9d75048fcdc75c0a4
Hamish Milne <hamishmilne83@gmail.com>
2020-02-22 13:48:52 +0000

    More complete json impl

--- Lexico.Test/Json.cs
+++ Lexico.Test/Json.cs
@@ -1,5 +1,8 @@
 #pragma warning disable CS0169,CS0649,IDE0044,IDE0051
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
 using Lexico;
 
 namespace Json
@@ -17,14 +20,33 @@ namespace Json
     public class JsonNumber : JsonValue
     {
         [Term] float value;
-        public float Value => value;
+
+        public override string ToString() => value.ToString();
     }
 
     public class JsonString : JsonValue
     {
         [Literal("\"")] Unnamed _;
-        [Regex("[^\"]*")] string value;
+        [Regex(@"(?:(?:\\.)|(\\u[a-fA-F0-9]{4})|[^""\\])*")] string value;
         [Literal("\"")] Unnamed __;
+
+        private static string Escapes(Match match)
+        {
+            return match.Value[1] switch {
+                'b' => "\b",
+                'f' => "\f",
+                'n' => "\n",
+                'r' => "\r",
+                't' => "\t",
+                '\\' => "\\",
+                '/' => "/",
+                '"' => "\"",
+                'u' => ((char)ushort.Parse(match.Value.AsSpan().Slice(1), NumberStyles.AllowHexSpecifier)).ToString(),
+                var x => throw new FormatException($"Invalid escape sequence: `{x}`")
+            };
+        }
+
+        public override string ToString() => Regex.Replace(value, @"\\(?:(?:u[a-fA-F0-9]{4})|.)", Escapes);
     }
 
     [WhitespaceSurrounded, MultiLine]
@@ -37,7 +59,7 @@ namespace Json
     public class JsonArray : JsonValue
     {
         [Literal("[")] Unnamed _;
-        [SeparatedBy(typeof(JsonSeparator))] List<JsonValue> values;
+        [SeparatedBy(typeof(JsonSeparator)), Optional] List<JsonValue> values;
         [Literal("]")] Unnamed __;
     }
 
@@ -53,7 +75,28 @@ namespace Json
     public class JsonDictionary : JsonValue
     {
         [Literal("{")] Unnamed _;
-        [SeparatedBy(typeof(JsonSeparator))] List<JsonProperty> properties;
+        [SeparatedBy(typeof(JsonSeparator)), Optional] List<JsonProperty> properties;
         [Literal("}")] Unnamed __;
+    }
+
+    public class JsonTrue : JsonValue
+    {
+        [Literal("true")] Unnamed _;
+
+        public override string ToString() => "true";
+    }
+
+    public class JsonFalse : JsonValue
+    {
+        [Literal("false")] Unnamed _;
+
+        public override string ToString() => "false";
+    }
+
+    public class JsonNull : JsonValue
+    {
+        [Literal("null")] Unnamed _;
+
+        public override string ToString() => "null";
     }
 }
