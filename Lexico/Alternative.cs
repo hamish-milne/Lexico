816bec76292e4cd11af1628baf43067222bf112f
Craig Jeffrey <CraigJeffrey3@gmail.com>
2020-02-23 17:23:20 +0000

    Add explicit alternatives and tests

--- Lexico/Alternative.cs
+++ Lexico/Alternative.cs
@@ -1,6 +1,8 @@
 using System.Linq;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+using static System.Reflection.BindingFlags;
 
 namespace Lexico
 {
@@ -10,26 +12,55 @@ namespace Lexico
     /// </summary>
     public class AlternativeAttribute : TermAttribute
     {
+        public AlternativeAttribute(params Type[] options) { Options = options; }
+        public Type[] Options { get; }
         public override int Priority => 10;
-        public override IParser Create(MemberInfo member, Func<IParser> child, IConfig config)
-        {
-            return new AlternativeParser(member.GetMemberType());
-        }
+        public override IParser Create(MemberInfo member, Func<IParser> child, IConfig config) =>
+            new AlternativeParser(member.GetMemberType(), Options);
 
         public override bool AddDefault(MemberInfo member)
             => member is Type t && (t.IsInterface || t.IsAbstract);
     }
+    
+    public class IndirectAlternativeAttribute : TerminalAttribute
+    {
+        public IndirectAlternativeAttribute(string property) {
+            Property = property ?? throw new ArgumentNullException(nameof(property));
+        }
+        public string Property { get; }
+
+        public override IParser Create(MemberInfo member, IConfig config)
+        {
+            var prop = member.ReflectedType.GetProperty(Property, Instance | Public | NonPublic)
+                       ?? throw new ArgumentException($"Could not find `{Property}` on {member.ReflectedType}");
+            return prop.GetValue(Activator.CreateInstance(member.ReflectedType, true)) is IEnumerable<Type> options
+                       ? new AlternativeParser(member.GetMemberType(), options)
+                       : throw new ArgumentNullException($"Found `{Property}` is not IEnumerable<Type>");
+        }
+    }
 
     internal class AlternativeParser : IParser
     {
-        public AlternativeParser(Type baseType)
+        public AlternativeParser(Type baseType, IEnumerable<Type> optionTypes = null)
         {
             this.baseType = baseType;
-            options = baseType.Assembly.GetTypes()
-                .Where(t => (t.IsClass || t.IsValueType) && !t.IsAbstract && baseType.IsAssignableFrom(t))
-                .Select(t => ParserCache.GetParser(t))
-                .ToArray();
-            if (options.Length == 0) {
+            if (optionTypes != null)
+            {
+                optionTypes = optionTypes as Type[] ?? optionTypes.ToArray();
+                foreach (var type in optionTypes)
+                {
+                    if (!baseType.IsAssignableFrom(type)) throw new ArgumentException($"Option '{type}' is not assignable to base type '{baseType}'");
+                }
+            }
+            else
+            {
+                optionTypes = baseType.Assembly.GetTypes().Where(t => (t.IsClass || t.IsValueType) && !t.IsAbstract && baseType.IsAssignableFrom(t));
+            }
+
+            // ReSharper disable once PossibleMultipleEnumeration
+            options = optionTypes.Select(ParserCache.GetParser).ToArray();
+            if (options.Length == 0)
+            {
                 throw new ArgumentException($"{baseType} has no concrete options");
             }
         }
