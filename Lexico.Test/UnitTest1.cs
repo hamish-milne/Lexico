129bc4eef3d41bea35f93c0ad23c730657c8b759
Craig Jeffrey <craig.jeffrey@ultraleap.com>
2020-02-27 17:15:16 +0000

    Split up tests, add repeat test

--- Lexico.Test/UnitTest1.cs
+++ Lexico.Test/UnitTest1.cs
@@ -1,4 +1,3 @@
-using System.Globalization;
 using Xunit;
 
 namespace Lexico.Test
@@ -23,72 +22,5 @@ namespace Lexico.Test
             }
             ");
         }
-
-        private class GetOnlyProp
-        {
-            [field:Term] public int Value { get; }
-        }
-
-        [Fact]
-        public void CannotWriteGetOnlyProperty() => Assert.NotEqual(5, Lexico.Parse<GetOnlyProp>("5").Value);
-
-        private class GetOnlyPropWithPrivateSet
-        {
-            [field:Term] public int Value { get; private set; }
-        }
-
-        [Fact]
-        public void WritePrivateSetProperty() => Assert.Equal(5, Lexico.Parse<GetOnlyPropWithPrivateSet>("5").Value);
-
-        private class ReadonlyField
-        {
-            [Term] public readonly int Value;
-        }
-
-        [Fact]
-        public void CannotWriteReadonlyField() => Assert.NotEqual(5, Lexico.Parse<ReadonlyField>("5").Value);
-
-        private class PrivateField
-        {
-            [Term] private int Value;
-            public int Val => Value;
-        }
-
-        [Fact]
-        public void WritePrivateField() => Assert.Equal(5, Lexico.Parse<PrivateField>("5").Val);
-
-        private class ProtectedField
-        {
-            [Term] protected int Value;
-            public int Val => Value;
-        }
-
-        [Fact]
-        public void WriteProtectedField() => Assert.Equal(5, Lexico.Parse<ProtectedField>("5").Val);
-
-        private class NonGenericSequence
-        {
-            [Whitespace] public int a;
-            [Term, NumberStyle(NumberStyles.None)] public int b;
-            [Whitespace] public int c;
-        }
-
-        private class GenericSequence<T>
-        {
-            [Whitespace] public Unnamed a;
-            [Term, NumberStyle(NumberStyles.None)] public T b;
-            [Whitespace] public Unnamed c;
-        }
-
-        private class GenericWrapper : GenericSequence<int> { }
-
-        [Fact]
-        public void OrderOfSequenceCorrect()
-        {
-            Assert.True(Lexico.TryParse(" 5 ", out NonGenericSequence _, new ConsoleTrace()));
-        }
-
-        [Fact]
-        public void OrderOfGenericSequenceCorrect() => Lexico.Parse<GenericWrapper>(" 5 ", new ConsoleTrace());
     }
 }
