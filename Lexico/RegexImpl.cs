7073ef55c71d685e66d2e3e2772926fc3972d0f9
Hamish Milne <hamishmilne83@gmail.com>
2020-06-14 16:22:45 +0100

    Turn off regex trace

--- Lexico/RegexImpl.cs
+++ Lexico/RegexImpl.cs
@@ -26,7 +26,7 @@ namespace Lexico.RegexImpl
             ? pattern.Create()
             : new ConcatParser(pattern.Create(), EOFParser.Instance));
 
-        public static IParser Parse(string pattern) => Lexico.Parse<Regex>(pattern, new ConsoleTrace{Verbose = true}).Create();
+        public static IParser Parse(string pattern) => Lexico.Parse<Regex>(pattern).Create();
     }
 
     public class SubstringParser : IParser
@@ -50,7 +50,8 @@ namespace Lexico.RegexImpl
 
     public class ConcatParser : IParser
     {
-        public ConcatParser(params IParser[] children) {
+        public ConcatParser(params IParser[] children)
+        {
             this.children = children;
         }
 
@@ -61,23 +62,29 @@ namespace Lexico.RegexImpl
         public void Compile(ICompileContext context)
         {
             Expression? sb = null;
-            if (context.Result != null) {
+            if (context.Result != null)
+            {
                 sb = context.Cache(New(typeof(StringBuilder)));
             }
             foreach (var c in children)
             {
                 Expression? output = null;
-                if (sb != null) {
+                if (sb != null)
+                {
                     output = context.Cache(Default(c.OutputType == typeof(char) ? typeof(char?) : typeof(string)));
                 }
                 context.Child(c, null, output, null, context.Failure);
-                if (sb != null) {
+                if (sb != null)
+                {
                     context.Append(Call(sb, nameof(StringBuilder.Append), Type.EmptyTypes, Convert(output, typeof(object))));
                 }
             }
-            if (sb != null) {
+            if (sb != null)
+            {
                 context.Succeed(Call(sb, nameof(StringBuilder.ToString), Type.EmptyTypes));
-            } else {
+            }
+            else
+            {
                 context.Succeed();
             }
         }
@@ -130,28 +137,32 @@ namespace Lexico.RegexImpl
         public override IParser Create() => inner.Create();
     }
 
-    public abstract class GroupModifier {}
+    public abstract class GroupModifier { }
 
-    public class NonCapturing : GroupModifier {
+    public class NonCapturing : GroupModifier
+    {
         [Literal("?")] Unnamed _;
         [CharSet(":>")] Unnamed __;
     }
 
-    public class Lookaround : GroupModifier {
+    public class Lookaround : GroupModifier
+    {
         [Literal("?")] Unnamed _;
         [Optional, Literal("<")] string? behind;
         [CharSet("=!")] char direction;
     }
 
-    public class Named : GroupModifier {
+    public class Named : GroupModifier
+    {
         [SurroundBy("?<", ">"), Repeat(Min = 1), CharRange("09", "AZ", "az", "__")]
         string name;
     }
 
-    public interface ISetItem {
+    public interface ISetItem
+    {
         CharIntervalSet Ranges { get; }
     }
-    
+
     public class CharRangePattern : ISetItem
     {
         [Term] SingleChar start;
@@ -160,8 +171,9 @@ namespace Lexico.RegexImpl
 
         public CharIntervalSet Ranges => new CharIntervalSet().Include(start.Value, end.Value);
     }
-    
-    public abstract class SingleChar : Pattern, ISetItem {
+
+    public abstract class SingleChar : Pattern, ISetItem
+    {
         public abstract char Value { get; }
         public CharIntervalSet Ranges => new CharIntervalSet().Include(Value, Value);
 
@@ -171,18 +183,20 @@ namespace Lexico.RegexImpl
         }
     }
 
-    public class HexChar : SingleChar {
+    public class HexChar : SingleChar
+    {
         [Literal("\\x")] Unnamed _;
         [CharRange("09", "AF", "af"), Repeat(Min = 2, Max = 2)] string hexValue;
         public override char Value => (char)byte.Parse(hexValue, NumberStyles.AllowHexSpecifier);
     }
 
-    public class ControlChar : SingleChar {
+    public class ControlChar : SingleChar
+    {
         [Literal("\\c")] Unnamed _;
         [CharRange("AZ")] char id;
         public override char Value => (char)(id - 'A' + 1);
     }
-    
+
     public class ExtendedUnicodeChar : SingleChar
     {
         [Literal("\\u")] Unnamed _;
@@ -190,19 +204,22 @@ namespace Lexico.RegexImpl
         public override char Value => (char)uint.Parse(hexValue, NumberStyles.AllowHexSpecifier);
     }
 
-    public class UnicodeChar : SingleChar {
+    public class UnicodeChar : SingleChar
+    {
         [Literal("\\u")] Unnamed _;
         [CharRange("09", "AF", "af"), Repeat(Min = 4, Max = 4)] string hex;
         public override char Value => (char)ushort.Parse(hex, NumberStyles.AllowHexSpecifier);
     }
 
-    public class OctalChar : SingleChar {
+    public class OctalChar : SingleChar
+    {
         [Literal("\\")] Unnamed _;
         [CharRange("07"), Repeat(Min = 3, Max = 3)] string octal;
-        public override char Value => (char)((octal[0]-'0')*8*8 + (octal[1]-'0')*8 + (octal[2]-'0'));
+        public override char Value => (char)((octal[0] - '0') * 8 * 8 + (octal[1] - '0') * 8 + (octal[2] - '0'));
     }
 
-    public class NumericRef : Pattern {
+    public class NumericRef : Pattern
+    {
         [Literal("\\")] Unnamed _;
         [CharRange("19")] char number;
 
@@ -227,14 +244,17 @@ namespace Lexico.RegexImpl
         // TODO: C# extensions like \p{}? https://docs.microsoft.com/en-us/dotnet/standard/base-types/regular-expression-language-quick-reference
         public CharIntervalSet Ranges
         {
-            get {
-                var set = char.ToLowerInvariant(id) switch {
-                    'w' => new CharIntervalSet().Include('a','z').Include('A','Z').Include('0','9').Include('_','_'),
-                    'd' =>  new CharIntervalSet().Include('0','9'),
-                    's' => new CharIntervalSet().Include('\t','\r').Include(' ',' '),
+            get
+            {
+                var set = char.ToLowerInvariant(id) switch
+                {
+                    'w' => new CharIntervalSet().Include('a', 'z').Include('A', 'Z').Include('0', '9').Include('_', '_'),
+                    'd' => new CharIntervalSet().Include('0', '9'),
+                    's' => new CharIntervalSet().Include('\t', '\r').Include(' ', ' '),
                     _ => throw new InvalidOperationException()
                 };
-                if (char.IsUpper(id)) {
+                if (char.IsUpper(id))
+                {
                     set.Invert();
                 }
                 return set;
@@ -248,7 +268,8 @@ namespace Lexico.RegexImpl
     {
         [Literal("\\")] Unnamed _;
         [Term] char c;
-        public override char Value => c switch {
+        public override char Value => c switch
+        {
             't' => '\t',
             'n' => '\n',
             'v' => '\v',
@@ -264,7 +285,7 @@ namespace Lexico.RegexImpl
     {
         [Term] Pattern inner;
         [Literal("{")] Unnamed _;
-        
+
         // TODO: make a regex-less Number 
         [CharRange("09"), Repeat] string min;
         [Literal(","), Optional] Unnamed __;
@@ -288,9 +309,11 @@ namespace Lexico.RegexImpl
         [Term] List<SetItem> items; // TODO: Min 1 or 0 here?
         [Literal("]")] Unnamed __;
 
-        public override IParser Create() {
+        public override IParser Create()
+        {
             var set = new CharIntervalSet(items.Select(i => i.item.Ranges));
-            if (invert != null) {
+            if (invert != null)
+            {
                 set.Invert();
             }
             return new CharSet(set);
@@ -304,7 +327,8 @@ namespace Lexico.RegexImpl
         public override IParser Create() => CharParser.Instance;
     }
 
-    public class RawChar : SingleChar {
+    public class RawChar : SingleChar
+    {
         [Not, CharSet(")|")] Unnamed _;
         [CharRange("\x20\uffff")] char value;
         public override char Value => value;
