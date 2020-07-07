6e962f5f498931cb2bdbb75aeb6b2bdaf2410d05
Craig Jeffrey <CraigJeffrey3@gmail.com>
2020-07-07 17:42:05 +0100

    Add UserObject type cast tests

--- Lexico.Test/UserObjectTests.cs
+++ Lexico.Test/UserObjectTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace Lexico.Test
@@ -8,6 +9,12 @@ namespace Lexico.Test
         {
             [Term] public int Number;
             [UserObject] public object UserObject;
+        }
+
+        public class ClassWithConcreteUserObject
+        {
+            [Term] public int Number;
+            [UserObject] public float UserObject;
         }
         
         [Fact]
@@ -24,6 +31,21 @@ namespace Lexico.Test
             var myUserObject = 3.14f;
             var obj = Lexico.Parse<ClassWithUserObject>("5", userObject: myUserObject);
             Assert.Equal(myUserObject, obj.UserObject);
+        }
+
+        [Fact]
+        public void CanUseConcreteTypeForUserObject()
+        {
+            var myUserObject = 3.14f;
+            var obj = Lexico.Parse<ClassWithConcreteUserObject>("5", userObject: myUserObject);
+            Assert.Equal(myUserObject, obj.UserObject);
+        }
+        
+        [Fact]
+        public void WrongUserObjectTypeThrowsCastException()
+        {
+            var myUserObject = 3;
+            Assert.Throws<InvalidCastException>(() => Lexico.Parse<ClassWithConcreteUserObject>("5", userObject: myUserObject));
         }
     }
 }
